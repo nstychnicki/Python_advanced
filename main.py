9cc817243f24e6ade86f8bdb80537d20c9387596
Nataly Stychnicki <stychnickinataly@gmail.com>
2023-07-26 19:10:33 -0300

    Aula 07 - funcao abs

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import random
 print("Bem vindo ao joguinho, tente adivinhar o número")
 
 numero_secreto = random.randrange(1,101)
-tentativas = 5
+tentativas = 0
+pontos = 100
 
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    tentativas = 20
+elif (nivel == 2):
+    tentativas = 10
+else:
+    tentativas = 5
 
 for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
     #interpolação de strings
+
     chute_str = input("Digite seu numero: ")
     print("voce digitou: ", chute_str)
     chute = int(chute_str)
 
-    if(chute<1 or chute>100):
+    if(chute < 1 or chute > 100):
         print("Digite apenas números entre 1 e 100")
         continue
 
@@ -23,12 +36,14 @@ for rodada in range(1, tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou! Você fez {} pontos".format(pontos))
         break
     else:
         if(maior):
             print("Você errou, chute um número menor!")
         elif(menor):
             print("Você errou, chute um número maior!")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do Jogo!")
