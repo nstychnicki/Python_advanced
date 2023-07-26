ab761efeab48365b6cb7748f9ee29f859bb4cc1a
Nataly Stychnicki <stychnickinataly@gmail.com>
2023-07-26 17:51:44 -0300

    Aula 05 - if, and, or

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
     #interpolação de strings
     chute_str = input("Digite seu numero: ")
-
     print("voce digitou: ", chute_str)
-
     chute = int(chute_str)
+
+    if(chute<1 or chute>100):
+        print("Digite apenas números entre 1 e 100")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -19,6 +21,7 @@ for rodada in range(1, tentativas + 1):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou, chute um número menor!")
