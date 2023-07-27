5c3ba13bfdd94d5321607379bbd5be54a4ae9f82
Nataly Stychnicki <stychnickinataly@gmail.com>
2023-07-27 19:15:20 -0300

    Aula 3/2 - Listas

--- forca.py
+++ forca.py
@@ -2,7 +2,11 @@ def jogo():
     print("Bem vindo ao joguinho!")
 
     palavra_secreta = "nataly"
+    letras_certas = ["_","_","_","_","_","_"]
+    #[] serve para criar listas
 
+    print(letras_certas)
+    
     enforcou = False
     acertou = False
     #precisa de letra maiuscula para definir booleano
@@ -12,13 +16,15 @@ def jogo():
         chute = input("Qual letra? ")
         chute = chute.strip()
         # entrada do usuario - string types
+        #não alteram a string original (string sao imutaveis)
 
         posicao = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, posicao + 1))
+                letras_certas[posicao] = letra
+                #substitui a letra certa dentro da lista
             posicao = posicao + 1
-
+        print(letras_certas)
     print("Fim do Jogo!")
 
 if (__name__ == "__main__"):
