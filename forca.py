a0be32b9fb07980739437ebe0344c57e960e170b
Nataly Stychnicki <stychnickinataly@gmail.com>
2023-08-01 21:51:55 -0300

    Aula 07/2 - criando funções

--- forca.py
+++ forca.py
@@ -1,9 +1,54 @@
 import random
-
-
 def jogo():
+    mensagem_boasvindas()
+    palavra_secreta = palavra_aleatoria()
+    letras_certas = inicializa_palavras(palavra_secreta)
+    #passa o parametro para que a função acesse a palavra_secreta
+
+    tentativa = 0
+
+    print(letras_certas)
+
+    enforcou = False
+    acertou = False
+    # precisa de letra maiuscula para definir booleano
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+        # entrada do usuario - string types
+        # strip - tira os espaços/ upper - deixa tudo em caps
+        # não alteram a string original (string sao imutaveis)
+
+        if (chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_certas[posicao] = letra
+                    # substitui a letra certa dentro da lista
+                posicao += 1
+
+        else:
+            tentativa += 1
+            print("Restam {} tentativas".format(6 - tentativa))
+
+        enforcou = tentativa == 6
+        # quando houver 6 tentativas o enforcou sera True
+        acertou = "_" not in letras_certas
+        # faz com que o programa pare quando não houver mais _
+        print(letras_certas)
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu :(")
+    print("Fim do Jogo.")
+
+def mensagem_boasvindas():
     print("Bem vindo ao joguinho!")
 
+def palavra_aleatoria():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -14,51 +59,15 @@ def jogo():
     arquivo.close()
 
     numero = random.randrange(0, len(palavras))
-    #randomiza dentro do range do arquivo txt
+    # randomiza dentro do range do arquivo txt
     palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
 
-    letras_certas = ["_" for letra in palavra_secreta]
-    #[] serve para criar listas
-    tentativa = 0
-
-    print(letras_certas)
-    
-    enforcou = False
-    acertou = False
-    #precisa de letra maiuscula para definir booleano
-
-    while(not enforcou and not acertou):
-
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
-        # entrada do usuario - string types
-        #strip - tira os espaços/ upper - deixa tudo em caps
-        #não alteram a string original (string sao imutaveis)
-
-        if(chute in palavra_secreta):
-            posicao = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_certas[posicao] = letra
-                    #substitui a letra certa dentro da lista
-                posicao += 1
-
-        else:
-            tentativa += 1
-            print("Restam {} tentativas" .format(6 - tentativa))
-
-        enforcou = tentativa == 6
-        #quando houver 6 tentativas o enforcou sera True
-        acertou = "_" not in letras_certas
-        #faz com que o programa pare quando não houver mais _
-        print(letras_certas)
-
-    if(acertou):
-        print("Você ganhou!")
-    else:
-        print("Você perdeu :(")
-    print("Fim do Jogo.")
+def inicializa_palavras(palavra):
+    return ["_" for letra in palavra]
+    # [] serve para criar listas
 
 if (__name__ == "__main__"):
     jogo()
+
 
